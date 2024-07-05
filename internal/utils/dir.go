a6142fa3af5e437e450b7935d29fd33377e70625
orewaee <orewaee@gmail.com>
2024-07-05 18:15:32 +0400

    feat: added util func for dir checking

---
+++ internal/utils/dir.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+// CheckDir creates a dir with the specified name if it does not exist. Returns an error if a file with the specified name exists.
+func CheckDir(name string) error {
+	info, err := os.Stat(name)
+
+	if err != nil && errors.Is(err, os.ErrNotExist) {
+		return os.Mkdir(name, 0666)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if !info.IsDir() {
+		text := fmt.Sprintf("%s is not a dir", name)
+		return errors.New(text)
+	}
+
+	return nil
+}

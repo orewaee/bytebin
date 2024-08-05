6f206e573ee12278dbe03058772a70a9f95ef735
orewaee <orewaee@gmail.com>
2024-08-05 18:01:24 +0400

    feat: added graceful shutdown

--- internal/adapters/http/http.go
+++ internal/adapters/http/http.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"context"
 	"github.com/orewaee/bytebin/internal/app/ports"
 	"github.com/rs/zerolog"
 	"net/http"
@@ -8,30 +9,37 @@ import (
 )
 
 type Server struct {
+	srv            *http.Server
 	bytebinService ports.BytebinService
 	log            *zerolog.Logger
 }
 
 func NewServer(bytebinService ports.BytebinService, log *zerolog.Logger) *Server {
+	mux := http.NewServeMux()
+
+	mux.Handle("POST /bin", NewPostHandler(bytebinService, log))
+	mux.Handle("GET /bin/{id}", NewGetHandler(bytebinService, log))
+
+	srv := &http.Server{
+		Handler:      mux,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+	}
+
 	return &Server{
+		srv:            srv,
 		bytebinService: bytebinService,
 		log:            log,
 	}
 }
 
 func (server *Server) Run(addr string) error {
-	mux := http.NewServeMux()
+	server.srv.Addr = addr
+	server.log.Info().Msgf("running server on addr %s...", addr)
+	return server.srv.ListenAndServe()
+}
 
-	mux.Handle("POST /bin", NewPostHandler(server.bytebinService, server.log))
-	mux.Handle("GET /bin/{id}", NewGetHandler(server.bytebinService, server.log))
-
-	srv := &http.Server{
-		Addr:         addr,
-		Handler:      mux,
-		ReadTimeout:  time.Second * 5,
-		WriteTimeout: time.Second * 5,
-	}
-
-	server.log.Info().Msgf("running app on addr %s", addr)
-	return srv.ListenAndServe()
+func (server *Server) Shutdown() error {
+	server.log.Info().Msg("stopping the server...")
+	return server.srv.Shutdown(context.TODO())
 }
